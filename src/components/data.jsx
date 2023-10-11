e5608d8a022456af18d0859c0d5c498fefb0a6e7
waweroh <scadmsez@gmail.com>
2023-10-11 16:42:57 +0300

    add service section data

--- src/components/data.jsx
+++ src/components/data.jsx
@@ -10,3 +10,27 @@ export  const socialLinks = [
   { id: 1, href: "https://www.twitter.com", icon: "fab fa-twitter" },
   { id: 1, href: "https://www.twitter.com", icon: "fab fa-squarespace" },
 ];
+
+export const services = [
+  {
+    id: 1,
+    icon: "fas fa-wallet fa-fw",
+    title: "saving money",
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, officia.",
+  },
+
+  {
+    id: 2,
+    icon: "fas fa-tree fa-fw",
+    title: "endless hiking",
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, officia.",
+  },
+
+  {
+    id: 3,
+    icon: "fas fa-socks fa-fw",
+    title: "amazing comfort",
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, officia.",
+  },
+
+];
